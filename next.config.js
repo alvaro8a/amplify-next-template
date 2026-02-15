26349055c87afa21f667e7c2b692a88e91430f53
alvaro8a <alvaro.ochoa@hotmail.com>
2026-02-15 15:23:19 +0100

    Update next.config 260215 1523 GPT5.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  assetPrefix: '/',
   images: {
     unoptimized: true,
   },
-  experimental: {},
-};
+  trailingSlash: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
