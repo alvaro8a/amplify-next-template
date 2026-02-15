c2de420a7376a0dbae778e8e6b86e80b01ec21e4
alvaro8a <alvaro.ochoa@hotmail.com>
2026-02-15 10:36:47 +0100

    Añadido next.config.js con output: 'export' para solucionar 404 en rutas.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',  // ← Esto fuerza static export (soluciona 404 en rutas)
+  images: {
+    unoptimized: true,  // Necesario para static export
+  },
+  trailingSlash: true,  // Ayuda a rutas como /login/
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
