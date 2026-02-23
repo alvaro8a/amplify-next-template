4269b898e1c202cd9661cf73b278d5b3e15ac74d
alvaro8a <alvaro.ochoa@hotmail.com>
2026-02-23 08:25:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
+  env: {
+    COGNITO_DOMAIN: process.env.COGNITO_DOMAIN,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_REDIRECT_URI: process.env.COGNITO_REDIRECT_URI,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+  },
 };
 
 module.exports = nextConfig;
